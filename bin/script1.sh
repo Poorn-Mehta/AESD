243f8d61dc7fc75ad0d0db6886b111cb470be431
Poorn Mehta <Poorn.Mehta@Colorado.EDU>
2019-01-24 00:33:51 -0700

    Third Commit

--- bin/script1.sh
+++ bin/script1.sh
@@ -9,11 +9,12 @@ lsb_release -i 1>>file1
 lsb_release -d 1>>file1
 lsb_release -r 1>>file1
 lsb_release -c 1>>file1
-printf "\nKernel Information\n\n" 1>>file1
+printf "\n******Kernel Information******\n\n" 1>>file1
 uname -s 1>>file1
 uname -r 1>>file1
 gcc --version 1>>file1
 uname -v 1>>file1
+printf "\n\n" 1>>file1
 echo "Architecture: `uname -m`" 1>>file1
-printf "\nFile System Memory\n\n" 1>>file1
+printf "\n******File System Memory******\n\n" 1>>file1
 df -h 1>>file1
